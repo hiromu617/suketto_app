53278e2f760a59b2f09bf109649cfca487677498
hiro <hiromhiro617@gmail.com>
2020-12-17 13:10:10 +0900

    setup app

---
+++ app/javascript/app.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <v-app-bar>
+      <v-toolbar-title>SUKETTO</v-toolbar-title>
+    </v-app-bar>
+    <p>{{ message }}</p>
+    <router-view>
+    </router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      message: "Hello!"
+    }
+  }
+}
+</script>
+
+<style scoped>
+p {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
---
+++ app/javascript/router/router.js
@@ -0,0 +1,28 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+import SignUp from '../components/SignUp'
+// import SignIn from '../components/SignIn'
+// import User from '../components/users/User'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignUp
+    },
+    // {
+    //   path: '/signin',
+    //   name: 'signin',
+    //   component: SignIn
+    // },
+    // {
+    //   path: '/api/users',
+    //   name: 'User',
+    //   component: User
+    // },
+  ]
+})
