5089961bd1a5cc643e1e5e98b70fb095fdd542da
hiro <hiromhiro617@gmail.com>
2021-01-15 14:33:24 +0900

    modified

--- app/javascript/plugins/axios.js
+++ app/javascript/plugins/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:3000"
-  // baseURL: "https://still-crag-34956.herokuapp.com"
+  // baseURL: "http://localhost:3000"
+  baseURL: "https://still-crag-34956.herokuapp.com"
 })
