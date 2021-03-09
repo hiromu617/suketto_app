fb5af7912206aa56b97b1558489e29b529157661
hiro <hiromhiro617@gmail.com>
2021-03-09 11:24:51 +0900

    commit

---
+++ app/javascript/packs/config.js
@@ -0,0 +1,11 @@
+const config = {
+  apiKey: "AIzaSyBsrOFZVr3xHcTDuCjMdIZBicdClAwI7jc",
+  authDomain: "suketto-app.firebaseapp.com",
+  projectId: "suketto-app",
+  storageBucket: "suketto-app.appspot.com",
+  messagingSenderId: "1055750055329",
+  appId: "1:1055750055329:web:c7585d2981dfe04faf17e0",
+  measurementId: "G-5N1LJRDRK4"
+};
+export default config 
+  
